3f43f200af2b30d5429259f7a81edeb4a8fadb54
NickyRatt17 <100880985+NickyRatt17@users.noreply.github.com>
2022-03-03 14:47:59 -0800

    Add files via upload

---
+++ Educated_humerus_2022_03_03_22_46_43/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight );
+
+
+
+
+}
+
+function draw() {
+  background("black");
+fill("white");
+  rect(mouseX, mouseY, 35, 35);
+  fill("white")
+  textSize(45)
+text("Fuck You Asshole", 250, 250);
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
